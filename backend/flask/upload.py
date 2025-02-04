a71bb1f8897f72e14d0e38d82c031678502e39dd
primerahour <hourprimera@gmail.com>
2025-02-04 11:12:11 -0700

    picture testing query

--- backend/flask/upload.py
+++ backend/flask/upload.py
@@ -160,11 +160,6 @@ def upload_match():
                             data.get("did_win"), data.get("game_number"), data.get("week")
                         )
                     )
-                    cursor.execute(
-                        picture_queries[game],
-                        game_id, data.get("game_number"), data.get("week"), data["school"],
-                        data["opponent_school"], data["w_points"], data["l_points"], data["image_url"]
-                    )
                 elif game == "apex-legends":
                     cursor.execute(
                         game_queries[game],
