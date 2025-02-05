de8d54292591285cc22ff981205b7d5fb7996ef1
Luke Anderson <llukeandersonn@gmail.com>
2025-02-05 09:32:24 -0700

    Recent Scoreboards and Layout Update

--- backend/ocr/RocketLeague/RLOCRMain.py
+++ backend/ocr/RocketLeague/RLOCRMain.py
@@ -14,6 +14,8 @@ def load_and_preprocess_image(img_path):
     img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
     return img_rgb, img_gray
 
+
+
 def extract_roi(img_gray, x, y, w, h):
     """Extract the region of interest (ROI) from the image."""
     roi_gray = img_gray[y:y+h, x:x+w]
@@ -32,15 +34,22 @@ def process_strip(strip, strip_n, strip_y, strip_h, strip_x, strip_w):
     """Process each strip (player's stats) within the ROI."""
     strip_gray = cv.GaussianBlur(strip, (3,3), 0)
     strip_gray = cv.adaptiveThreshold(strip_gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 5, -2)
-    strip_name = strip_n[:35, :300]
-    strip_stats = strip_gray[10:, 390:]
-    strip_score = open_strip(strip_stats[5:, :100])
-    cv.imshow("SCORE", strip_score)
+    strip_name = strip_n[:27, :290]
+    cv.imshow("Name", strip_name)
+    strip_stats = strip_gray[:, 330:]
+    cv.imshow("Stats", strip_stats)
     cv.waitKey(0)
-    strip_goals = open_strip(strip_stats[5:, 115:175])
-    strip_assists = open_strip(strip_stats[5:, 200:275])
-    strip_saves = open_strip(strip_stats[5:, 320:380])
-    strip_shots = open_strip(strip_stats[5:, 410:])
+    strip_score = open_strip(strip_stats[:, :65])
+    cv.imshow("Score", strip_score)
+    strip_goals = open_strip(strip_stats[:, 95:150])
+    cv.imshow("Goals", strip_goals)
+    strip_assists = open_strip(strip_stats[:, 185:230])
+    cv.imshow("Assits", strip_assists)
+    strip_saves = open_strip(strip_stats[:, 275:320])
+    cv.imshow("Saves", strip_saves)
+    strip_shots = open_strip(strip_stats[:, 360:])
+    cv.imshow("Shots", strip_shots)
+    cv.waitKey()
     return strip_name, strip_score, strip_goals, strip_assists, strip_saves, strip_shots
 
 def perform_ocr(strip, config):
@@ -94,7 +103,7 @@ def save_results(img_result, players, filename):
     return json_file
 
 def main():
-     #usr for vm, homebrew for mac, program files for Windows
+    #usr for vm, homebrew for mac, program files for Windows
     #pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract' # For VM
     #pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract' # For Mac
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' # For Windows
@@ -110,17 +119,18 @@ def main():
     img_rgb, img_gray = load_and_preprocess_image(img_path)
     players = defaultdict(lambda: ('-1', '-1', '-1', '-1', '-1'))
 
-    x, y, w, h = 670, 280, 925, 420
+    x, y, w, h = 725, 275, 730, 355
     roi_gray, roi_gray_n = extract_roi(img_gray, x, y, w, h)
     img_result = img_rgb.copy()
 
     for i in range(6):
-        strip_x = 67
-        strip_y = [5, 60, 120, 260, 320, 375][i]
-        strip_w = 843
-        strip_h = 40
+        strip_x = 0
+        strip_y = [0, 60, 107, 235, 282, 332][i]
+        strip_w = 725
+        strip_h = 35
         strip = roi_gray[strip_y:strip_y + strip_h, strip_x:strip_x + strip_w]
-        #cv.imshow("strip", strip)
+        cv.imshow("strip", strip)
+        cv.waitKey(0)
         strip_n = roi_gray_n[strip_y:strip_y + strip_h, strip_x:strip_x + strip_w]
 
         strip_name, strip_score, strip_goals, strip_assists, strip_saves, strip_shots = process_strip(strip, strip_n, strip_y, strip_h, strip_x, strip_w)
