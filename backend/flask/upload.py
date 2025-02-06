185d6cd882a92dbc27d75c2d3dff7d74472eda82
primerahour <hourprimera@gmail.com>
2025-02-06 01:11:51 -0700

    working

--- backend/flask/upload.py
+++ backend/flask/upload.py
@@ -160,7 +160,7 @@ def upload_match():
                 cursor.execute(
                         picture_queries[game],
                         (
-                            game_id, data.get("game_number"), data.get("week"), data["school"],
+                            game_id, data.get("game_number",1), data.get("week"), data["school"],
                             data["opponent_school"], data["w_points"], data["l_points"], data["image_url"]
                         )
                     )
