e401761b1de76df95ef606438e36f17ea20ef36a
Mustafa Sameen <s_sameen@coloradocollege.edu>
2025-02-11 09:57:57 -0700

    Update ModifyPage.jsx

--- frontend/src/components/ModifyPage.jsx
+++ frontend/src/components/ModifyPage.jsx
@@ -173,13 +173,15 @@ const ModifyPage = () => {
             className="mx-2 mb-4 w-[48%] rounded-md border border-custom-off-white bg-custom-gray p-4 text-white"
             onChange={(e) => handleInputChange(e, "school")}
           />
-          <input
-            type="text"
-            value={formData.opponent_school}
-            placeholder="Opponent School"
-            className="mx-2 mb-4 w-[48%] rounded-md border border-custom-off-white bg-custom-gray p-4 text-white"
-            onChange={(e) => handleInputChange(e, "opponent_school")}
-          />
+          {formData.game == "valorant" && (
+            <input
+              type="text"
+              value={formData.opponent_school}
+              placeholder="Opponent School"
+              className="mx-2 mb-4 w-[48%] rounded-md border border-custom-off-white bg-custom-gray p-4 text-white"
+              onChange={(e) => handleInputChange(e, "opponent_school")}
+            />
+          )}
           {formData.game == "valorant" && (
             <div>
               <input
