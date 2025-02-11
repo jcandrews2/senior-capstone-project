75113d7dd2d68dc99b0f7fb459f5b7c0b8153f6f
shamdeed30 <117672677+shamdeed30@users.noreply.github.com>
2025-02-10 19:18:31 -0700

    New VM IP

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
-const API_BASE_URL = "https://40.85.147.30:8080";
+//const API_BASE_URL = "https://40.85.147.30:8080";
+const API_BASE_URL = "http://128.85.24.19:8080";
 // For local testing uncomment this and comment the above line
-// const API_BASE_URL = "http://127.0.0.1:8080";
+//const API_BASE_URL = "http://127.0.0.1:8080";
 
 const API_ENDPOINTS = {
   handleGetPlayerStats: (videogame, playerName) =>
